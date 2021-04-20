d9cc86a28371d50652c9f3a75110b89e8ce12348
Antonio <antonio.lucas@trt4.jus.br>
2021-04-19 23:06:03 -0300

    [#6] lint .eslintrc.js (didn't think it was necessary)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'ckeditor5',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    "indent": ["error", 4],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'ckeditor5'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        'indent': [ 'error', 4 ]
+    }
 };
